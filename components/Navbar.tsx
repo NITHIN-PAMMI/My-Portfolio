0a84a4aa0e0e167f87057db3bde004f52a9e7d3e
NITHIN-PAMMI <pamminithin@gmail.com>
2025-09-27 10:36:13 +0530

    #2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar() {
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-muted">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5 text-xl font-semibold">
-              Nithin Pammi
+              NITHIN PAMMI
             </a>
             <Button
               variant="ghost"
